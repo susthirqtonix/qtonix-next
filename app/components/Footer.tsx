ba918883d5ea8a1ff0ec09feabf030b023afca2b
Susthir <susthir19march@gmail.com>
2026-04-27 11:23:07 +0530

    basic setup done

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white p-4 mt-10">
+      <p>© 2026 Qtonix</p>
+    </footer>
+  )
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-black text-white p-4">
+      <h2>Qtonix</h2>
+    </header>
+  )
+}
